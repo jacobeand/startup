0d1a4436891a1ea3aeebaf3538e3b611488ca22d
Jacob <jacobeanderson@yahoo.com>
2023-03-10 23:28:40 -0700

    added sorting capabilities

--- database.js
+++ database.js
@@ -1,31 +1,40 @@
-let allRatings= JSON.parse(localStorage.getItem("allRatings")) || [];
+$(document).ready( function () {
+    $('#sortTable').DataTable();
+} );
+function loadRatings() {
 
-const tableBodyEl = document.querySelector('#database');
 
-if(allRatings.length) {
-    const rowH = document.createElement('th')
-    for (const[i, rating] of allRatings) {
-        const nameTdEl = document.createElement('td');
-        const foodNameTdEl = document.createElement('td');
-        const foodTypeTdEl = document.createElement('td');
-        const ratingValTdEl = document.createElement('td');
-        const restaurantNameTdEl = document.createElement('td');
+    let allRatings = JSON.parse(localStorage.getItem("allRatings")) || [];
 
-        let fName = rating.firstName;
-        let lName = rating.lastName.charAt(0);
-        nameTdEl.textContent = `${fName} ${lName}.`;
-        foodNameTdEl.textContent = rating.foodName;
-        foodTypeTdEl.textContent = rating.foodType;
-        ratingValTdEl.textContent = rating.ratingVal;
-        restaurantNameTdEl.textContent = rating.restaurantName;
+    const tableBodyEl = document.querySelector('#database');
 
-        const rowEl = document.createElement('tr');
-        rowEl.appendChild(foodTypeTdEl);
-        rowEl.appendChild(foodNameTdEl);
-        rowEl.appendChild(ratingValTdEl);
-        rowEl.appendChild(restaurantNameTdEl);
-        rowEl.appendChild(nameTdEl);
+    if (allRatings.length) {
+        for (const rating of allRatings) {
+            const nameTdEl = document.createElement('td');
+            const foodNameTdEl = document.createElement('td');
+            const foodTypeTdEl = document.createElement('td');
+            const ratingValTdEl = document.createElement('td');
+            const restaurantNameTdEl = document.createElement('td');
+
+            let fName = rating.firstName;
+            let lName = rating.lastName.charAt(0);
+            nameTdEl.textContent = `${fName} ${lName}.`;
+            foodNameTdEl.textContent = rating.foodName;
+            foodTypeTdEl.textContent = rating.foodType;
+            ratingValTdEl.textContent = rating.ratingVal;
+            restaurantNameTdEl.textContent = rating.restaurantName;
+
+            const rowEl = document.createElement('tr');
+            rowEl.appendChild(foodTypeTdEl);
+            rowEl.appendChild(foodNameTdEl);
+            rowEl.appendChild(ratingValTdEl);
+            rowEl.appendChild(restaurantNameTdEl);
+            rowEl.appendChild(nameTdEl);
+
+            tableBodyEl.appendChild(rowEl);
+        }
+    } else {
+        tableBodyEl.innerHTML = '<tr><td colSpan=5> Be the first to Leave a rating!</td></tr>'
     }
-} else {
-    tableBodyEl.innerHTML = '<tr><td colSpan=4> Be the first to Leave a rating!</td></tr>'
 }
+loadRatings();
